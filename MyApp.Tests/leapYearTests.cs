b32901b1e2b78302a5153b7eec81108c0c398681
thekure <laku@itu.dk>
2022-09-06 14:28:46 +0200

    finished up to and including exercise 5

--- MyApp.Tests/leapYearTests.cs
+++ MyApp.Tests/leapYearTests.cs
@@ -3,20 +3,90 @@ namespace MyApp.Tests;
 public class leapYearTests
 {
     [Fact]
-    public void rename()
+    public void isLeapYear_Should_Return_true_On_Inputs_4_100_400()
     {
         // Arrange
-        var Year = new Year();
+        var results = new List<bool>();
 
          // Act
-        var results = new List<bool>();
-        results.Add(Year.isLeapYear(4));
-        results.Add(!Year.isLeapYear(100));
-        results.Add(Year.isLeapYear(400));
+        results.Add(Year.isLeapYear(4000));
+        results.Add(!Year.isLeapYear(1900));
+        results.Add(Year.isLeapYear(40000));
 
         // Assert
         foreach (bool test in results){
             test.Should().Be(true);
         }
     }
+
+    [Fact]
+    public void printIsLeapYearShouldThrowFormatException()
+    {
+        // Arrange
+            var intstring = "hello";
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+        // Act
+            Year.printIsLeapYear(intstring);
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Cannot be parsed to an integer.");
+       
+    }
+
+    [Fact]
+    public void printIsLeapYearShouldPrintYay()
+    {
+        // Arrange
+            var intstring = "4000";
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+        // Act
+            Year.printIsLeapYear(intstring);
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("yay");
+       
+    }
+
+    [Fact]
+    public void printIsLeapYearShouldPrintNay()
+    {
+        // Arrange
+            var intstring = "1900";
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+        // Act
+            Year.printIsLeapYear(intstring);
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("nay");
+       
+    }
+
+[Fact]
+    public void isLeapYearShouldThrowExceptionUnder1582()
+    {
+        // Arrange
+            var testIntString = "1581";
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+        // Act
+            Year.printIsLeapYear(testIntString);
+            
+            
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Does not support years before 1582");
+       
+    }
+    
 }
